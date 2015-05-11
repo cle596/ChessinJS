aa267edef198c6263b5373ad1926c9645c4f76c2
changhan <changhanlee@gmail.com>
2015-05-11 15:50:25 -0400

    pad

--- tess.js
+++ tess.js
@@ -1,19 +1,68 @@
-var board="rnbqkbnr/pppppppp/eeeePPPPPPPP/RNBQKBNR/"
+var readline = require('readline');
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: true
+});
+
+var board=
+  "//////////"+
+  "/rnbqkbnr/"+
+  "/pppppppp/"+
+  "/......../"+
+  "/......../"+
+  "/......../"+
+  "/......../"+
+  "/PPPPPPPP/"+
+  "/RNBQKBNR/"+
+  "//////////"
 
 function print_board(board){
-  str="";
-  for (i=0;i<board.length;++i){
-    if (board[i]=='/'){
-      console.log(str);
-      str="";
-    }
-    else if (board[i]=='e'){
-      console.log(Array(9).join('. '));
-    }
-    else{
-      str+=board[i]+" ";
-    }
+  offset=11;
+  i=offset;
+  while(i<board.length-offset+1){
+    s="";
+    s=board.substr(i,8).split("").join(" ");
+    console.log(s);
+    i+=10;
   }
+  return board;
 }
 
-print_board(board);
+function eval_board(board){
+  score=0;
+  ///material
+  key={
+    "p":1,"n":3,"b":5,"r":7,"q":20,"k":100,
+    "P":-1,"N":-3,"B":-5,"R":-7,"Q":-20,"K":-100
+  };
+  for (x=0;x<board.length;++x){
+    for (property in key){
+      if (board[x]==property){
+        score+=key[property];
+      }
+    }
+  }
+  return score;
+}
+
+function prompt_move(){
+  ret=""
+  console.log("your move: ");
+  rl.resume();
+  rl.on('line', function(line){
+    ret=line;
+    rl.pause();
+  });
+  return ret;
+}
+
+
+
+function main_loop(board){
+  print_board(board);
+  eval_board(board);
+  prompt_move();
+}
+
+main_loop(board);
